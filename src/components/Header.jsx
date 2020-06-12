6fc299fa2e235f8555eb7fc0a4452b82fafb41f9
Samip Poudel <samippdl17@gmail.com>
2020-06-12 14:09:22 +0545

    changed css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Header() {
-  return <header>KeepMate</header>;
+  return (
+    <header>
+      <h1>KeepMate</h1>
+      <p>Samip Poudel © 2020</p>
+    </header>
+  );
 }
 
 export default Header;

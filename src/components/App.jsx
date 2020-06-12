a5ff89392bdd96bd2a9b472bf0fa1c8bac9e12ca
Samip Poudel <samippdl17@gmail.com>
2020-06-12 17:35:25 +0545

    Added local storage feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
 import Header from "./Header";
-import Footer from "./Footer";
 import Note from "./Note";
 import TextArea from "./TextArea";
 
 function App() {
-  let [lists, setLists] = useState([]);
+  let mydata = JSON.parse(localStorage.getItem("notes"));
+  let [lists, setLists] = useState(mydata === null ? [] : mydata);
+
   function handleClick(inp) {
     setLists((prevItems) => {
+      let localList = [...prevItems, inp];
+      localStorage.setItem("notes", JSON.stringify(localList));
       return [...prevItems, inp];
     });
   }
 
   function handleDelete(id) {
     setLists((prevItems) => {
+      let local = prevItems.filter((item, index) => {
+        return index !== id;
+      });
+      localStorage.setItem("notes", JSON.stringify(local));
       return prevItems.filter((item, index) => {
         return index !== id;
       });
